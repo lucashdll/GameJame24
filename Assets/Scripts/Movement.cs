7309c838522fc84b1130d13b900198e2cc4c57d0
UPO652 <upo652@usask.ca>
2024-09-21 03:08:22 -0600

    added L&R movement as well as jump and isGrounded method & "Ground" tag on floor

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    private Rigidbody2D body;
     private Transform player;
 
-    [SerializeField]
-    public float moveSpeed;
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public float jumpHeight;
+    
 
-    [SerializeField]
-    public GameObject boundaries;
+    [SerializeField] public GameObject boundaries;
+    private bool isGrounded;
+    private void Awake(){
+
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +29,31 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //stores player facing direction for ez access
+        float horizontalInput = Input.GetAxis("Horizontal"); 
+
+
+        //left and right movement multiplied by movement speed
+        body.velocity = new Vector2(horizontalInput*moveSpeed, body.velocity.y);
+        //press spacebar to jump
+        if (Input.GetKey(KeyCode.Space) && isGrounded){
+            Jump();
+        }
+
         
+    
+    }
+
+    private void Jump(){
+
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+
+        if (collision.gameObject.tag == "Ground"){
+            isGrounded = true;
+        }
     }
 }
