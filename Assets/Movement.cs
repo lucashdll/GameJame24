f176ebafc39768e8df7708649dec2c5e7bac85e0
Lucas <ldh029@usask.ca>
2024-09-21 01:32:10 -0600

    player movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Transform player;
+
+    [SerializeField]
+    public float moveSpeed;
+
+    [SerializeField]
+    public GameObject boundaries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
