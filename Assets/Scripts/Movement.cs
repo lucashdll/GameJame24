4ecd9d5a425a4342563a0b21a02d739c4ccf1a8d
UPO652 <upo652@usask.ca>
2024-09-21 18:14:15 -0600

    Cleaned up sloppy code, added comments and organized it before I redo the gawd damn wall jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,28 +7,43 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody2D body;
     private Transform player;
-
+    private BoxCollider2D boxCollider;
+    [SerializeField] public GameObject boundaries;
+//Layers
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+// basic movement stuff
     [SerializeField] public float moveSpeed;
     [SerializeField] public float jumpHeight;
-    [SerializeField] public float slamSpeed;
-
-
-
-   
-[SerializeField] public float dashTime;
-[SerializeField] public float dashSpeed;
-public float time;
-
-
-
-public float isDashing;
-    
-    [SerializeField] public GameObject boundaries;
+    [SerializeField] public float groundDetectionDistance = 0.1f;
+    [SerializeField] public float wallDetectionDistance = 420f;
+    private float horizontalInput;
     private bool isGrounded;
+    
+// wall jump stuff
+    public float wallJumpHeight = 10;
+    public float wallJumpSpeed = 10;
+    private float wallDirection;
+    private bool onWall;
+    private float walljumpTimer = 200000;
+// groundslam stuf
+
+// removed dash stuff
+   
+
+    
+    
+    
+    
+    
+    
+    
+    
     private void Awake(){
 
         body = GetComponent<Rigidbody2D>();
-        time = 0;
+        
+        boxCollider = GetComponent<BoxCollider2D>();
         
     }
 
@@ -44,61 +59,83 @@ public float isDashing;
     void Update()
     {
         //stores player facing direction for ez access
-        float horizontalInput = Input.GetAxis("Horizontal"); 
+        horizontalInput = Input.GetAxis("Horizontal"); //values -1, 0, 1 for determining direction player is facing
 
-        if (Input.GetKey(KeyCode.LeftShift)){
-            
-        }
+        //checks if player walljumped, sets velocity back to normal after
+        walljumpTimer -= Time.deltaTime;
+        if (walljumpTimer < 0){
         //left and right movement multiplied by movement speed
-        body.velocity = new Vector2(horizontalInput * moveSpeed, body.velocity.y);
-        
+        body.AddForce(new Vector2(horizontalInput * moveSpeed, body.velocity.y));
+        wallDirection = -horizontalInput;
+        }
         
         
         
         
         //press spacebar to jump
-        if (Input.GetKey(KeyCode.Space) && isGrounded){
+        if (Input.GetKey(KeyCode.Space)){
             Jump();
         }
+        
         //trigger groundslam
-        if (Input.GetKey(KeyCode.S) && (isGrounded == false)){
-            GroundSlam();
-        }
+        // if (Input.GetKey(KeyCode.S) && (isGrounded == false)){
+            // GroundSlam();
+        // }
         
         
-        if (Input.GetKey(KeyCode.LeftShift)){
-           Dash(horizontalInput);
-        }
-        else if(!Input.GetKey(KeyCode.LeftShift)){
-            body.velocity = new Vector2(horizontalInput * moveSpeed, body.velocity.y);
-        }
+        
 
     
     }
 
-    private void Jump(){
+    private bool IsGrounded(){//raycast check to see if on ground returns T/F
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+        
+    }
+    private bool OnWall(){//raycast check to see if close to wall
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 420f, wallLayer);
+        return raycastHit.collider != null;
+    }
 
+
+    private void Jump(){//called when space is pressed, checks if player close to ground or wall
+
+        if (IsGrounded()){//regular jump
         body.velocity = new Vector2(body.velocity.x, jumpHeight);
         isGrounded = false;
+        }
+        else if (OnWall() && !IsGrounded()){//walljump
+            // if(horizontalInput != 0){
+            
+            body.AddForce(new Vector2(Mathf.Sign(wallDirection * wallJumpSpeed) * 2000, 1000));
+            transform.localScale = new Vector2(-Mathf.Sign(transform.localScale.x), transform.localScale.y);
+            body.velocity = new Vector2(wallJumpSpeed * 2, wallJumpHeight*2);
+            walljumpTimer = 2000000;
+            // }
+            
+        }
     }
+    
 
     private void OnCollisionEnter2D(Collision2D collision){
 
         if (collision.gameObject.tag == "Ground"){
             isGrounded = true;
         }
+        if (collision.gameObject.tag == "Wall"){
+            onWall = true;
+            
+        }
     }
 
-    private void GroundSlam(){
-        body.velocity = new Vector2(0, slamSpeed);
+    //private void GroundSlam(){
+    //    body.velocity = new Vector2(0, slamSpeed);
         
-    }
+   // }
     
-    private void Dash(float horizontalInput){
-         body.velocity = new Vector2(horizontalInput * dashSpeed,body.velocity.y);
-    } 
+    
         
-    
     
 
 }
